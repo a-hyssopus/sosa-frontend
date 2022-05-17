9740fb30745cdad636dce8f78526c3e9b88c4d02
Daria Balutel <ceban.daria2000@gmail.com>
2022-05-17 14:11:47 +0200

    Switched to version 17 due to React Quill peer dependencies error with React 18

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,39 @@ import React from "react"
 import "antd/dist/antd.css";
 
 import './App.scss'
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navigation/Navbar";
+import {Route, Routes} from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage";
+import OurStories from "./components/OurStories/OurStories";
+import Story from "./components/OurStories/Story";
 
 const App = () => (
-    <div className='App'/>
+    <div className='App'>
+        <Header/>
+        <Navbar/>
+        <Routes>
+            <Route path={"/our-stories"} element={<OurStories/>}/>
+            <Route path={"/our-stories/:id"} element={<Story/>}/>
+            <Route path={"/home"} element={<MainPage/>}/>
+            <Route exact path={"/"} element={<MainPage/>}/>
+        </Routes>
+    </div>
 )
+
+// const App = () => {
+//     return (
+//         <div className="app">
+//             <Routes>
+//                 <HomeLayoutRoute path="/" element={<Home />} />
+//                 <PrivateRoute path="/" element={<PrivateScreen/>} />
+//                 <Route path="/login" element={<LoginScreen/>} />
+//                 <Route path="/register" element={<RegisterScreen/>} />
+//                 <Route path="/forgotpassword" element={<ForgotPasswordScreen/>}/>
+//                 <Route path="/passwordreset/:resetToken" element={<ResetPasswordScreen/>}/>
+//             </Routes>
+//         </div>
+//     );
+// };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import {createRoot} from "react-dom/client";
+import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { store } from "./store/store"
+import {store} from "./store/store"
 import {Provider} from 'react-redux'
-
 
 import './index.scss';
 import App from './App';
-import MainPage from "./components/MainPage/MainPage";
-import OurStories from "./components/OurStories/OurStories"
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navigation/Navbar";
-import Story from "./components/OurStories/Story";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<BrowserRouter>
-    <Provider store={store}>
-        <Header/>
-        <Navbar/>
-        <Routes>
-            <Route exact path={"/"} element={<MainPage/>}/>
-            <Route exact path={"/home"} element={<MainPage/>}/>
-            <Route path={"/our-stories"} element={<OurStories/>}/>
-            <Route path={"/our-stories/:id"} element={<Story/>}/>
-        </Routes>
-        <App/>
-    </Provider></BrowserRouter>);
+ReactDOM.render(<BrowserRouter>
+        <Provider store={store}>
+            <Routes>
+                <Route path="*" element={<App/>}/>
+            </Routes>
+        </Provider></BrowserRouter>,
+    document.getElementById("root")
+);
+//
