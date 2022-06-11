478e951c9c3e6f91f6765b784f1f00c15e873e14
Daria Balutel <ceban.daria2000@gmail.com>
2022-06-12 00:04:32 +0200

    Carousel animation fixed

--- src/components/MainPage/Carousel/CustomCarousel.jsx
+++ src/components/MainPage/Carousel/CustomCarousel.jsx
@@ -10,34 +10,31 @@ import "./style.scss"
 const CustomCarousel = () => {
     const images = [dog, kittens, chainedDog];
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const [carouselItemClass, setCarouselItemClass] = useState("carousel-item")
-
-    setTimeout(() => {
-        setCarouselItemClass("carousel-item carousel-item-active")
-    }, 400)
+    const [carouselItemClass, setCarouselItemClass] = useState("carousel-item");
 
     const onClickNextHandler = () => {
         setCarouselItemClass(prev => prev === "carousel-item" ? "carousel-item carousel-item-active" : "carousel-item");
         setTimeout(() => {
             setCurrentImageIndex(prevIndex => prevIndex !== images.length - 1 ? prevIndex + 1 : 0)
-            setCarouselItemClass("carousel-item carousel-item-active")
-        }, 400)
+        }, 300)
     }
 
     const onClickPreviousHandler = () => {
         setCarouselItemClass(prev => prev === "carousel-item" ? "carousel-item carousel-item-active" : "carousel-item");
         setTimeout(() => {
             setCurrentImageIndex(prevIndex => prevIndex !== 0 ? prevIndex - 1 : images.length - 1)
-            setCarouselItemClass("carousel-item carousel-item-active")
-        }, 400)
+        }, 300)
     }
-
 
     return (
         <div className="carousel-container">
             <div className="carousel-item carousel-item-background">
-                <img className={carouselItemClass} src={images[currentImageIndex]}/>
-                <img className="carousel-control-arrow" src={arrow} onClick={onClickNextHandler}/>
+                <img className={carouselItemClass} src={images[currentImageIndex]} onLoad={() => {
+                    setCarouselItemClass("carousel-item carousel-item-active")
+                }}/>
+                <img className="carousel-control-arrow" src={arrow}
+                     onClick={onClickNextHandler}
+                />
                 <img className="carousel-control-arrow carousel-control-arrow--back" src={arrow}
                      onClick={onClickPreviousHandler}/>
             </div>
